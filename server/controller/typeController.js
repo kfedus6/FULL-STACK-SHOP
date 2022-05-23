ccd191518fa6f92e42d4762686a9a87102fda05b
fedus <feduskolya@gmail.com>
2022-05-23 16:01:46 +0300

    Sample code

--- server/controller/typeController.js
+++ server/controller/typeController.js
@@ -2,21 +2,21 @@ const ApiError = require('../error/apierror');
 const { Type } = require('../models/models');
 
 class TypeController {
-   async create(req, res, next) {
-      const { name } = req.body;
-      if (name == undefined) {
-         return next(ApiError.badRequest('incorrect json'))
-      } else if (name == "") {
-         return next(ApiError.badRequest('name undefined'))
-      }
-      const type = await Type.create({ name })
-      return res.json({ type })
-   }
+    async create(req, res, next) {
+        const { name } = req.body;
+        if (name == undefined) {
+            return next(ApiError.badRequest('incorrect json'))
+        } else if (name == "") {
+            return next(ApiError.badRequest('name undefined'))
+        }
+        const type = await Type.create({ name })
+        return res.json({ type })
+    }
 
-   async getType(req, res) {
-      const type = await Type.findAll()
-      return res.json({ type })
-   }
+    async getType(req, res) {
+        const type = await Type.findAll()
+        return res.json({ type })
+    }
 };
 
 const typeController = new TypeController();
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,49 +4,49 @@ const bcrypt = require('bcrypt');
 const ApiError = require('../error/apierror');
 
 class UserController {
-   async registration(req, res, next) {
-      const { email, password, admin } = req.body
-      if (email == undefined) {
-         return next(ApiError.badRequest('incorrect email'))
-      } else if (password == undefined) {
-         return next(ApiError.badRequest('incorrect password'))
-      }
+    async registration(req, res, next) {
+        const { email, password, admin } = req.body
+        if (email == undefined) {
+            return next(ApiError.badRequest('incorrect email'))
+        } else if (password == undefined) {
+            return next(ApiError.badRequest('incorrect password'))
+        }
 
-      const findUser = await User.findOne({ where: { email } })
+        const findUser = await User.findOne({ where: { email } })
 
-      if (findUser !== null) {
-         return next(ApiError.badRequest('this email is registered'))
-      }
+        if (findUser !== null) {
+            return next(ApiError.badRequest('this email is registered'))
+        }
 
-      const bcryptPassword = await bcrypt.hash(password, 5)
-      const user = await User.create({ email: email, password: bcryptPassword, admin: admin })
-      const basket = await Basket.create({ userId: user.id })
+        const bcryptPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({ email: email, password: bcryptPassword, admin: admin })
+        const basket = await Basket.create({ userId: user.id })
 
-      const token = jwt.sign({ email }, process.env.codeSecret, { expiresIn: '1h' })
-      res.json({ token })
-   }
+        const token = jwt.sign({ email }, process.env.codeSecret, { expiresIn: '1h' })
+        res.json({ token })
+    }
 
-   async login(req, res) {
-      const { email, password } = req.body
-      const emailUser = await User.findOne({ where: { email } })
-      if (emailUser == null) {
-         return next(ApiError.notFound('this email is not registered'))
-      }
+    async login(req, res) {
+        const { email, password } = req.body
+        const emailUser = await User.findOne({ where: { email } })
+        if (emailUser == null) {
+            return next(ApiError.notFound('this email is not registered'))
+        }
 
-      let resPassword = bcrypt.compareSync(password, emailUser.password)
+        let resPassword = bcrypt.compareSync(password, emailUser.password)
 
-      if (resPassword == false) {
-         return next(ApiError.badRequest('incorrect password'))
-      }
+        if (resPassword == false) {
+            return next(ApiError.badRequest('incorrect password'))
+        }
 
-      const token = jwt.sign({ email: email, admin: emailUser.admin }, process.env.codeSecret, { expiresIn: '1h' })
-      res.json({ token })
-   }
+        const token = jwt.sign({ email: email, admin: emailUser.admin }, process.env.codeSecret, { expiresIn: '1h' })
+        res.json({ token })
+    }
 
-   async authorization(req, res) {
-      const token = jwt.sign({ email }, process.env.codeSecret, { expiresIn: '1h' })
-      res.json({ token })
-   }
+    async authorization(req, res) {
+        const token = jwt.sign({ email }, process.env.codeSecret, { expiresIn: '1h' })
+        res.json({ token })
+    }
 };
 
 const userController = new UserController();
